fec1a32732e21868b64fa137d3cd4ad863243410
Navarjun_office <navarjun.singh@gmail.com>
2018-04-18 14:06:56 -0400

    filtering data for unique sleep ids

--- sleep-client-worker.js
+++ sleep-client-worker.js
@@ -1,6 +1,11 @@
 onmessage = function(e) {
     var data = e.data[0];
-    var sleepSessionIds = [...new Set(data.map(function(d){ return d.id; }))];
-
-    console.log(data, sleepSessionIds);
+    var sleepSessionIds = [...new Set(data.map(function(d){ return d.id; }))]
+        .filter(function(d) { return d; });
+    var updatedData = [];
+    for (var i = 0; i < sleepSessionIds.length; i++) {
+        var id = sleepSessionIds[i];
+        updatedData.push(data.filter(function(d) { return d.id === id; })[0]);
+    }
+    console.log(updatedData, sleepSessionIds);
 }
